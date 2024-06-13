7b8d0210d0831923738c3549acb8b93a9d371fc4
denniswachira <dennismutitu06@gmail.com>
2024-06-13 17:45:56 +0000

    wahh

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -7,10 +7,6 @@ SELECT
 		WHEN split IS NOT NULL THEN split - formed
 		ELSE 2022 - formed
 	END AS lifespan
-
-FROM
-	metal_bands
-WHERE
-	main_style = 'Glam rock'
-ORDER BY
-	lifespan DESC;
+FROM metal_bands
+WHERE STYLE LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
